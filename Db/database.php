16f57c1aa3f72861ed5036aae6f55c781a1ea7d6
Luiz Reis <reisluiz05@gmail.com>
2022-10-28 08:08:29 -0300

    Estrutura inicial do projeto

---
+++ Db/database.php
@@ -0,0 +1,28 @@
+<?php
+
+class Database
+{
+    const HOST = 'localhost';
+    const NAME = 'todo';
+    const USER = 'root';
+    const PASS = '';
+
+    private $table;
+    private $connection;
+
+    public function __construct($table = null)
+    {
+        $this->table = $table;
+        $this->setConnection();
+    }
+
+    private function setConnection()
+    {
+        try {
+            $this->connection = new PDO('mysql:host=' . self::HOST . ';dbname=' . self::NAME, self::USER, self::PASS);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die('ERROR: ' . $e->getMessage());
+        }
+    }
+}
