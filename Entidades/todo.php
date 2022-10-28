da07ce881f7d7f22b26d949c8f96b74bffaa0413
luiz <reisluiz05@gmail.com>
2022-10-28 08:55:54 -0300

    Estrutura da entidade principal

---
+++ Entidades/todo.php
@@ -0,0 +1,64 @@
+<?php
+
+class Todo
+{
+
+    private $id;
+    private $descricao;
+    private $data;
+    private $status;
+
+    public function __construct($id = null, $descricao = null, $data = null, $status = null)
+    {
+        $this->id = $id;
+        $this->descricao = $descricao;
+        $this->data = $data;
+        $this->status = $status;
+    }
+
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getDescricao()
+    {
+        return $this->descricao;
+    }
+
+    public function setDescricao($descricao)
+    {
+        $this->descricao = $descricao;
+        return $this;
+    }
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    public function setData($data)
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
